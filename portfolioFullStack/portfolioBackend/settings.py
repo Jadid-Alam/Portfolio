054ca7a1235dc56c86ab8ea5842029efb055a846
Jadid Alam <jadid.alam.08@gmail.com>
2024-10-16 09:54:39 +0100

    Author: Jadid Alam Date: 2024-10-16 Version: 2.4
    - changed the settings.py file to allow vercel app to run

--- portfolioFullStack/portfolioBackend/settings.py
+++ portfolioFullStack/portfolioBackend/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3000",'.vercel.app',
 ]
 
 CORS_ALLOW_METHODS = [
